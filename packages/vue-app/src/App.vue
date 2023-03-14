9e743cb3eddf83269fb9376da9dd15b85d9b9ee2
xbzhang2020 <xbzhang2020@icloud.com>
2023-03-14 22:50:54 +0800

    feat(root): comon

--- packages/vue-app/src/App.vue
+++ packages/vue-app/src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import lib from 'lib'
+import common from 'common'
 
-console.log(lib)
+console.log(common)
 </script>
 
 <template>
